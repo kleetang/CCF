d69be9b0a0b31891f0c35f8d077f027c28f18306
Klee <kleetang@gmail.com>
2014-07-28 16:26:59 -0400

    Add alumni.php

---
+++ alumni.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alumni</title>
+    <link rel="icon" href="img/logo2.png" type="image/png">
+    <!-- Bootstrap -->
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- header -->
+    <div id="header">
+      <?php 
+        include("./share/head.php");
+      ?>
+    </div>
+    <!-- main -->
+    <div id="main-wrapper">  
+      <div id="main">
+        <div class="container">
+          <!-- title -->
+          <div class="row">
+              <div class="col-md-4">
+                <div class="title-background"><br></div>
+              </div>
+              <div class="col-md-4 middle-title">
+                <div class="title1"><h2>CCF校友</h2></div>
+              </div>
+              <div class="col-md-4">
+                <div class="title-background"><br></div>
+              </div>
+          </div>
+          <div class="row margin_26_10">
+            <div class="col-xs-12 content2">
+              <p>Coming Up Soon!</p>
+            </div>
+          </div>
+      </div>
+    </div><!-- wrapper -->
+    <!-- footer -->
+    <?php 
+      include("./share/footer.php");
+    ?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/jquery-1.11.1.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <script src="./js/all.min.js"></script>
+  </body>
+</html>
