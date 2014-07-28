be65da292d2a9a8d4813f12d0faa53200e94175c
Klee <kleetang@gmail.com>
2014-07-28 16:16:41 -0400

    Add photos.php

---
+++ photos.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Photos</title>
+    <link rel="icon" href="img/logo2.png" type="image/png">
+    <!-- Bootstrap -->
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- header -->
+    <div id="header">
+      <?php 
+        include("./share/head.php");
+      ?>
+    </div>
+    <!-- main -->
+    <div id="main-wrapper">  
+      <div id="main">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <iframe scrolling="no" marginheight="0" frameborder="0" width="721" style="height: 505px;" src="http://facebookgalleria.com/gallery.php?id=2955898578322&display=album&rows=4&margin=10&cols=4&width=170&title_color=000000&hide_next_back=0&share_buttons=1&shape=rectangle&frame=1&token=85084062-07a1-11e4-ac90-2e2c531c9183"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- wrapper -->
+    <!-- footer -->
+    <?php 
+      include("./share/footer.php");
+    ?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <script src="./js/all.min.js"></script>
+  </body>
+</html>
