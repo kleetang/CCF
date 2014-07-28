8715e2a4bc7e9e2d3cca616de9e26e036a42a387
Klee <kleetang@gmail.com>
2014-07-28 16:22:16 -0400

    Add index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CCF石溪大學團契</title>
+    <link rel="icon" href="img/logo2.png" type="image/png">
+    <!-- Bootstrap -->
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+
+</head>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- header -->
+    <div id="header">
+      <?php 
+        include("./share/head.php");
+      ?>
+    </div>
+    <!-- main -->
+    <div id="main-wrapper">  
+      <div id="main">
+        <div class="row">
+          <div class="col-xs-12">
+
+
+          </div>
+        </div>
+      </div>
+    </div><!-- wrapper -->
+    <!-- footer -->
+    <?php 
+      include("./share/footer.php");
+    ?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <script src="./js/all.min.js"></script>
+    <script src="./js/jquery-1.11.1.js"></script>
+  </body>
+</html>
