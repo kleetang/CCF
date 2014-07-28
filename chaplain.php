86f078338d398d91c73c593fb559623fd012be4d
Klee <kleetang@gmail.com>
2014-07-28 16:29:42 -0400

    Add chaplain.php

---
+++ chaplain.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chaplain</title>
+    <link rel="icon" href="img/logo2.png" type="image/png">
+    <!-- Bootstrap -->
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- header -->
+
+    <div id="header">
+      <?php 
+        include("./share/head.php");
+      ?>
+    </div>
+    <!-- main -->
+    <div id="main-wrapper">  
+      <div id="main">
+        <div class="container">
+          <div class="row">
+              <div class="col-md-4">
+                <div class="title-background"><br></div>
+              </div>
+              <div class="col-md-4 middle-title">
+                <div class="title2"><h2>Chaplains</h2></div>
+              </div>
+              <div class="col-md-4">
+                <div class="title-background"><br></div>
+              </div>
+          </div>
+          <!-- first -->
+          <div class="row margin_t_20 margin_b_20">
+            <div class="col-xs-12 col-sm-4">
+              <div class="profilepic margin_b_20">
+                <img class="img-responsive center-block" src="img/pic/SerenaLin.jpg">
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+              <div class="row margin_b_10">
+                <h2 class="col-xs-12 text-center margin_t_0">
+                  林恂 牧師 (Rev. Dr. Serena Lin)
+                </h2>
+              </div>
+              <div class="row margin_l_10 margin_r_10">
+                <div class="col-xs-12 content1">
+                  <div class="row">
+                    <div class="col-sm-4 col-lg-2 text-center">
+                      <p>曾任：</p>
+                    </div>
+                    <div class="col-sm-8 col-lg-10 text-left">
+                      <p>紐約州立石溪大學 (State University of New York at Stony Brook)校牧十一年</p>
+                      <p>牧養紐約長島基督福音教會十年</p>
+                      <p>2004年，杜克大學醫學中心 (Duke University Medical Center)院牧</p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-4 col-lg-2  text-center">
+                      <p>曾獲： </p>
+                    </div>
+                    <div class="col-sm-8 col-lg-10 text-left">
+                      <p>1991年按立牧職</p>
+                      <p>Doctor of Divinity (D. Min. +)</p>
+                      <p>Duke Medical Center - Clinical Patient Counselor</p>
+                      <p>1999美南浸信會東岸校牧獎；2000 美南浸信會全美校牧獎</p>
+                      <p>出版譯、著十三本</p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-4 col-lg-2  text-center">
+                    <p>現任：  </p>
+                    </div>
+                    <div class="col-sm-8 col-lg-10 text-left">
+                    <p>E Mission World 執行主任</p>
+                    <p>紐約州立石溪大學亞裔校牧Chairperson</p>
+                    <p>基督使者協會董事 校園顧問  特約講員</p>
+                    <p>中華福音神學院(台北)北美董事會</p>
+                    <p>北美中華福音神學院(加州) Academic Consultant</p>
+                    </div>
+                  </div>
+
+                  My Prayer
+                  “ No Reserves, No Retreats, No Regrets.”
+                  不保留，不退縮，不後悔
+
+                    <p>「林姐」是CCF稱林恂牧師特有的專利「暱名」；因當時的林姐只比研究生長幾歲；她於’90年接續了CCF的傳道，招世超牧師之職；’92年一月，經過了十次裡裡外外的面談，她進入BCM校牧室，成為Director of BCM,也是Stony Brook的校牧成員；從此CCF也隨著林姐，進入BCM的陽傘下；尤其是’90年代後期，校方因人數增加對社團限制非常多，然CCF因著與BCM的銜接，能夠不受影響的迅速成長，成為一個屬靈的大家庭；週五晚，聚會滿到窗台座；GC我們以150人為底線；林姐工作表是七天的；也把一生最寶貴的青春奉獻給石溪的校園事工。她說：那是我無怨無悔的獻上，也是我事奉主的生命中至深的祝福！ </p>
+                    <p>2003年，林姐以為該自學生事工中退休了，在經過兩年的等候與禱告後，遂赴NC，開始發展推廣神學教育的跨教會宣道生活；沒有任何差會或教會或神學院的支持，只有幾位校友，憑信心地走過了另一decade. 至於CCF呢？2006年，BCM更換人事，改換宗旨；林姐回來與校方商談，之後成立亞裔校牧室(ACCM)；CCF此後換了把陽傘；林姐人仍舊作那打傘服事CCF的幕後支持者；她說：校園事工原來是我一生的呼召…</p>
+                    <br>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container">
+            <hr class="featurette-divider break-line">
+          </div>
+          <!-- second -->
+          <div class="row margin_t_20 margin_b_20">
+            <div class="col-xs-12 col-sm-4 col-sm-push-8">
+              <div class="profilepic margin_b_20">
+                <img class="img-responsive center-block" src="img/123.jpg">
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-sm-pull-4">
+              <div class="row margin_b_10">
+                <h2 class="col-xs-12 text-center margin_t_0">
+                  黃喆牧師
+                </h2>
+              </div>
+              <div class="row margin_l_10 margin_r_10">
+                <div class="col-xs-12 content1">
+                    <p>Chinese Christian Fellowship 簡稱 CCF 的中文名稱是「石溪大學團契」。 我們建立於八十年代初期。CCF 聚集校內許多的華人學生，幫助他們認識及了解基督教、聖經及基督徒的生活。 我們的社團主要成員大部份來自香港、中國大陸及台灣。</p>
+                    <p>1991年開始CCF由Baptist Campus Ministry贊助，2010年開始CCF由Asian Christian Campus Ministry贊助。在Stony Brook University Union，CCF有兩間辦公室。</p>
+                    <p>CCF 不只是一般的校內組織，它不僅給了大家一個認識新朋友的機會，更讓大家的心靈變得充實！ 我們非常歡迎新的朋友來參與我們的聚會！每一個星期五晚上我們將會有團契活動：除了詩歌分享及茶點外， 每一次 CCF 亦會邀請不同的資深講員來提供講座，內容大多環繞基督徒信仰上及生活上的信息。</p>
+                    <p>CCF 亦有小組聚會，分為國語及廣東兩個小組，在每一個星期三的晚上舉行。可以給大家更多的機會互相分享及關懷各人的屬靈生活。 可按自己意願選擇適合的小組來參加。小組是以詩歌、遊戲、查經及個人分享讓大家對聖經，自己，即彼此有更深的認識。</p>
+                    <p>好！那就請大家一同來與我們發掘聖經的智慧及我們的信仰吧！</p>
+                    <br>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container">
+            <hr class="featurette-divider break-line">
+          </div>
+          <!-- third -->
+          <div class="row margin_t_20 margin_b_20">
+            <div class="col-xs-12 col-sm-4">
+              <div class="profilepic margin_b_20">
+                <img class="img-responsive center-block" src="img/123.jpg">
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+              <div class="row margin_b_10">
+                <h2 class="col-xs-12 text-center margin_t_0">
+                  黃喆牧師
+                </h2>
+              </div>
+              <div class="row margin_l_10 margin_r_10">
+                <div class="col-xs-12 content1">
+                    <p>Chinese Christian Fellowship 簡稱 CCF 的中文名稱是「石溪大學團契」。 我們建立於八十年代初期。CCF 聚集校內許多的華人學生，幫助他們認識及了解基督教、聖經及基督徒的生活。 我們的社團主要成員大部份來自香港、中國大陸及台灣。</p>
+                    <p>1991年開始CCF由Baptist Campus Ministry贊助，2010年開始CCF由Asian Christian Campus Ministry贊助。在Stony Brook University Union，CCF有兩間辦公室。</p>
+                    <p>CCF 不只是一般的校內組織，它不僅給了大家一個認識新朋友的機會，更讓大家的心靈變得充實！ 我們非常歡迎新的朋友來參與我們的聚會！每一個星期五晚上我們將會有團契活動：除了詩歌分享及茶點外， 每一次 CCF 亦會邀請不同的資深講員來提供講座，內容大多環繞基督徒信仰上及生活上的信息。</p>
+                    <p>CCF 亦有小組聚會，分為國語及廣東兩個小組，在每一個星期三的晚上舉行。可以給大家更多的機會互相分享及關懷各人的屬靈生活。 可按自己意願選擇適合的小組來參加。小組是以詩歌、遊戲、查經及個人分享讓大家對聖經，自己，即彼此有更深的認識。</p>
+                    <p>好！那就請大家一同來與我們發掘聖經的智慧及我們的信仰吧！</p>
+                    <br>
+                </div>
+              </div>
+            </div>
+          </div>         
+        </div>
+        <div class="back">
+          <a href="#" class="back-to-top">Top</a>
+        </div>
+      </div>
+    </div><!-- wrapper -->
+    <!-- footer -->
+    <?php 
+      include("./share/footer.php");
+    ?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <script src="./js/all.min.js"></script>
+  </body>
+</html>
